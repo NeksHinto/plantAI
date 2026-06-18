d031ab69c90ffb0bc985b3450139e3a06cc57efe
NeksHinto <nicole1810@hotmail.com>
2026-06-18 00:17:45 -0300

    [Function] Add `loadComponent` functionality to prevent repeating code and reuse global components.

---
+++ frontend/functions/include.js
@@ -0,0 +1,17 @@
+async function loadComponents() {
+  const components = document.querySelectorAll("[data-include]");
+
+  await Promise.all(
+    [...components].map(async (component) => {
+      const response = await fetch(component.dataset.include);
+
+      if (!response.ok) {
+        throw new Error(`No se pudo cargar ${component.dataset.include}`);
+      }
+
+      component.outerHTML = await response.text();
+    })
+  );
+}
+
+document.addEventListener("DOMContentLoaded", loadComponents);
