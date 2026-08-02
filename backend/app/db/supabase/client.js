9716302a5a431238e8878682dba7289637559a75
NeksHinto <nicole1810@hotmail.com>
2026-08-02 11:11:32 -0300

    [Supabase Integration] Remove unecessary `ws` library for realtime devmode

--- backend/app/db/supabase/client.js
+++ backend/app/db/supabase/client.js
@@ -1,14 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-import ws from "ws";
 
 export const supabase = createClient(
   process.env.PUBLIC_SUPABASE_URL,
-  process.env.PUBLIC_SUPABASE_ANON_KEY,
-  {
-    realtime: { transport: ws },
-  }
+  process.env.PUBLIC_SUPABASE_ANON_KEY
 );
-
-export function getPublicStorageUrl(bucket, path) {
-  return `${process.env.PUBLIC_SUPABASE_URL}/storage/v1/object/public/${bucket}/${path}`;
-}
